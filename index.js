2c59fdc729f16d80cfda115c71c50baae83d968c
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-02-07 20:10:03 -0300

    feat: initial attempt validation logic

--- index.js
+++ index.js
@@ -14,10 +14,38 @@ const main = async () => {
     
     await page.mouse.click(0, 0);
 
-    await page.keyboard.type('Teste');
+    const word = 'Teste'
+
+    await page.keyboard.type(word);
     await page.keyboard.press('Enter');
 
+    const element = await page.waitForSelector('[aria-label="palavra 1"]')
+    const value = await element.evaluate(el => {
+        return el.innerHTML
+    })
+
+    const awnsers = value.replace(/\s\s+/g, '').split('/div>').filter(Boolean)
+
+    const attempt = awnsers.map((awnser, index) => {
+        const accurate = checksLetter(awnser)
+
+        return {
+            letter: word[index],
+            position: index,
+            accurate
+        }
+    })
+    
+    console.log(attempt)
+
     await browser.close()
+}
+
+const checksLetter = str => {
+    if (str.includes('letter wrong')) return 'wrong'
+    if (str.includes('letter place')) return 'place'
+    
+    return 'right'
 }
 
 main().catch(e => console.log(e))
